4a1d9164f81a84156ce5d01ba2bebe7e8d511473
김선혜 <shye.kim@kt.com>
2021-05-17 22:56:30 +0900

    [Fix] merge 누락 오류 수정

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import 'react-native-gesture-handler';
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {CameraRoll, BarcodeScanner , Ranking} from './src/screen';
+import {CameraRoll, BarcodeScanner  ,Board, Ranking} from './src/screen';
 
 const navOptionHandler = () => ({
   headerShown: false,
